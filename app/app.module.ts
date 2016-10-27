7b685f1a8cda2981c9c3208dcf27437e9f215f54
Francisco Moya <francisco.moya@uclm.es>
2016-10-27 13:23:59 +0200

    Final version of tutorial

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,25 +11,29 @@ import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroSearchComponent }   from './hero-search.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroService }          from './hero.service';
 
+import './rxjs-extensions';
+
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-  ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+    imports: [
+	BrowserModule,
+	FormsModule,
+	HttpModule,
+	InMemoryWebApiModule.forRoot(InMemoryDataService),
+	AppRoutingModule
+    ],
+    declarations: [
+	AppComponent,
+	DashboardComponent,
+	HeroDetailComponent,
+	HeroesComponent,
+	HeroSearchComponent
+    ],
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
